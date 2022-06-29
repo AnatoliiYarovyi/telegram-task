c331a08fce22cb7092c9aad03e5889a6ecd3f63b
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-06-29 15:18:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,83 @@
 process.env.NTBA_FIX_319 = 1;
-import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
 dotenv.config();
-import { getWeather } from "./functions/getWeather.js";
-import getWindInformation from "./functions/getWindInformation.js";
+
+import { getWeather } from './functions/getWeather.js';
+import getWindInformation from './functions/getWindInformation.js';
+import { getDataMono, getExchangeRates } from './functions/getExchangeRates.js';
+getDataMono();
 
 const { TOKEN } = process.env;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome", {
-    reply_markup: {
-      keyboard: [["/Weather"], ["/Coin"]],
-    },
-  });
+	bot.sendMessage(msg.chat.id, 'Welcome', {
+		reply_markup: {
+			keyboard: [['/Weather'], ['/Coin']],
+		},
+	});
 });
 
 bot.onText(/\/Weather/, (msg) => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Оберіть інтервал часу або дізнайтесь швидкість вітру",
-    {
-      reply_markup: {
-        keyboard: [
-          ["Кожні 3 години", "Кожні 6 годин"],
-          ["Вітер"],
-          ["Попередне меню"],
-        ],
-      },
-    }
-  );
+	bot.sendMessage(
+		msg.chat.id,
+		'Оберіть інтервал часу або дізнайтесь швидкість вітру',
+		{
+			reply_markup: {
+				keyboard: [
+					['Кожні 3 години', 'Кожні 6 годин'],
+					['Вітер'],
+					['Попередне меню'],
+				],
+			},
+		},
+	);
 });
 
-bot.on("message", async (msg) => {
-  if (!msg.entities) {
-    const chatId = msg.chat.id;
+bot.onText(/\/Coin/, (msg) => {
+	bot.sendMessage(msg.chat.id, 'Оберіть валюту яка Вас цікавить', {
+		reply_markup: {
+			keyboard: [['USD', 'EUR'], ['Попередне меню']],
+		},
+	});
+});
 
-    switch (msg.text) {
-      case "Кожні 3 години":
-        bot.sendMessage(chatId, await getWeather(3));
-        break;
+bot.on('message', async (msg) => {
+	if (!msg.entities) {
+		const chatId = msg.chat.id;
 
-      case "Кожні 6 годин":
-        bot.sendMessage(chatId, await getWeather(6));
-        break;
+		switch (msg.text) {
+			case 'Кожні 3 години':
+				bot.sendMessage(chatId, await getWeather(3));
+				break;
 
-      case "Вітер":
-        bot.sendMessage(chatId, await getWindInformation());
-        break;
+			case 'Кожні 6 годин':
+				bot.sendMessage(chatId, await getWeather(6));
+				break;
 
-      case "Попередне меню":
-        bot.sendMessage(msg.chat.id, "Welcome", {
-          reply_markup: {
-            keyboard: [["/Weather", "/Coin"]],
-          },
-        });
-        break;
+			case 'Вітер':
+				bot.sendMessage(chatId, await getWindInformation());
+				break;
 
-      default:
-        console.log("Invalid subscription type");
-    }
-  }
+			case 'USD':
+				bot.sendMessage(chatId, await getExchangeRates('USD'));
+				break;
+
+			case 'EUR':
+				bot.sendMessage(chatId, await getExchangeRates('EUR'));
+				break;
+
+			case 'Попередне меню':
+				bot.sendMessage(msg.chat.id, 'Welcome', {
+					reply_markup: {
+						keyboard: [['/Weather', '/Coin']],
+					},
+				});
+				break;
+
+			default:
+				console.log('Invalid subscription type');
+		}
+	}
 });
